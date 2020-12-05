190f8e371cf9eb51035b603d391af0254ca9dcd6
Neehar Sawant <neehar2@linux-a2.ews.illinois.edu>
2020-12-05 14:02:50 -0600

    updated main

--- main.cpp
+++ main.cpp
@@ -6,12 +6,14 @@
 #include <vector>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include "graph.h"
 
 using namespace std;
-//  ROSHUN IS GAY
-void parseAirports(ifstream &airnames, map<string, Airports> &airportData);
-void parseRoutes(ifstream &routes, map<string,Airports> &airportData);
+
+void parseAirports(ifstream &airnames, map<string, Airports> &airportData, map<string, string> &airportNames);
+void parseRoutes(ifstream &routes, map<string,Airports> &airportData, map<string, string> &airportNames);
+double findDist(double srclong, double srclat, double destlong, double destlat);
 
 int main(int argc, char *argv[]){
 
@@ -23,7 +25,7 @@ int main(int argc, char *argv[]){
     ifstream routes;
     ifstream airnames;
 
-    routes.open("test.dat");
+    routes.open("testR.dat");
     airnames.open("airports.dat");
     parseAirports(airnames, airportData, airportNames);
     parseRoutes(routes, airportData, airportNames);
@@ -35,8 +37,8 @@ int main(int argc, char *argv[]){
 void parseAirports(ifstream &airnames, map<string, Airports> &airportData, map<string, string> &airportNames){
     vector<string> result;
     string line;
-    string longitude;
-    string latitude;
+    string strlongitude;
+    string strlatitude;
     string airportName;
     string iata;
     Airports newport;
@@ -46,17 +48,25 @@ void parseAirports(ifstream &airnames, map<string, Airports> &airportData, map<s
         while(getline(ss, line, ',')){
             result.push_back(line);
         }
-        longitude = result[6];
-        latitude = result[7];
-        airportName = result[1];
-        iata = result[4];
-        newport.longitude = longitude;
-        newport.latitude = latitude;
-        newport.airportName = airportName;
-        newport.iata = iata;
+        if(iata != "\"N/A\""){
+            strlongitude = result[6];
+            strlatitude = result[7];
+            airportName = result[1];
+            iata = result[4];
 
-        airportNames[iata] = airportName;
-        airportData[airportName] = newport;
+            iata.erase(0, 1);
+            iata.erase(iata.size() - 1);
+            airportName.erase(0, 1);
+            airportName.erase(airportName.size() - 1);
+
+            newport.longitude = std::stod(strlongitude);
+            newport.latitude = std::stod(strlatitude);
+            newport.airportName = airportName;
+            newport.iata = iata;
+
+            airportNames[iata] = airportName;
+            airportData[airportName] = newport;
+        }
         //cout << longitude << "  "<< latitude << "  ";
     }
 }
@@ -73,8 +83,46 @@ void parseRoutes(ifstream &routes, map<string,Airports> &airportData, map<string
         }
         srcAirportID = result[2];
         destAirportID = result[4];
-        double longitude = airportData[airportNames[srcAirportID]].longitude;
-        
-        // cout << srcAirportID << "  "<< destAirportID << "  ";
+        double srclong = airportData[airportNames[srcAirportID]].longitude;
+        double srclat = airportData[airportNames[srcAirportID]].latitude;
+
+        double destlong = airportData[airportNames[destAirportID]].longitude;
+        double destlat = airportData[airportNames[destAirportID]].latitude;
+        double distance = findDist(srclong, srclat, destlong, destlat);
+
+        cout << srcAirportID << "  "<< destAirportID << "  " << distance;
     }
 }
+
+double findDist(double srclong, double srclat, double destlong, double destlat){
+    long double one_deg = (M_PI) / 180; 
+
+    // Convert the latitudes  
+    // and longitudes 
+    // from degree to radians. 
+    double lat1 = srclat * one_deg;
+    double long1 = srclong * one_deg; 
+    double lat2 = destlat * one_deg; 
+    double long2 = destlong * one_deg; 
+      
+    // Haversine Formula 
+    long double dlong = long2 - long1; 
+    long double dlat = lat2 - lat1; 
+  
+    long double ans = pow(sin(dlat / 2), 2) +  
+                          cos(lat1) * cos(lat2) *  
+                          pow(sin(dlong / 2), 2); 
+  
+    ans = 2 * asin(sqrt(ans)); 
+  
+    // Radius of Earth in  
+    // Kilometers, R = 6371 
+    // Use R = 3956 for miles 
+    long double R = 6371; 
+      
+    // Calculate the result 
+    ans = ans * R; 
+  
+    return ans;
+}
+
