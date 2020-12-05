d1d8e0be8d87260e67574e09e3b6c60bed0bd79d
Roshun Navin <rnavin2@illinois.edu>
2020-12-05 11:16:49 -0600

    changed main a bit

--- main.cpp
+++ main.cpp
@@ -9,23 +9,26 @@
 #include "graph.h"
 
 using namespace std;
-void parseRoutes(ifstream &routes, map<string,string> &airportRoutes){
-    vector<string> result;
-    string line;
-    string srcAirportID;
-    string destAirportID;
-    while(getline(routes, line)){
-        result.clear();
-        stringstream ss(line);
-        while(getline(ss, line, ',')){
-            result.push_back(line);
-        }
-        srcAirportID = result[2];
-        destAirportID = result[4];
 
-        
-        // cout << srcAirportID << "  "<< destAirportID << "  ";
-    }
+void parseAirports(ifstream &airnames, map<string, Airports> &airportData);
+void parseRoutes(ifstream &routes, map<string,Airports> &airportData);
+
+int main(int argc, char *argv[]){
+
+    map<string, Airports> airportData;
+    map<string, string> airportRoutes;
+
+
+    ifstream routes;
+    ifstream airnames;
+
+    routes.open("test.dat");
+    airnames.open("airports.dat");
+    parseAirports(airnames, airportData);
+    parseRoutes(routes, airportData);
+
+    
+    return 0;
 }
 
 void parseAirports(ifstream &airnames, map<string, Airports> &airportData){
@@ -56,70 +59,21 @@ void parseAirports(ifstream &airnames, map<string, Airports> &airportData){
         //cout << longitude << "  "<< latitude << "  ";
     }
 }
+void parseRoutes(ifstream &routes, map<string,Airports> &airportData){
+    vector<string> result;
+    string line;
+    string srcAirportID;
+    string destAirportID;
+    while(getline(routes, line)){
+        result.clear();
+        stringstream ss(line);
+        while(getline(ss, line, ',')){
+            result.push_back(line);
+        }
+        srcAirportID = result[2];
+        destAirportID = result[4];
 
-
-int main(int argc, char *argv[]){
-
-    map<string, Airports> airportData;
-    map<string, string> airportRoutes;
-
-
-    ifstream routes;
-    ifstream airnames;
-
-    routes.open("test.dat");
-    airnames.open("airports.dat");
-    parseAirports(airnames, airportData);
-    parseRoutes(routes, airportRoutes);
-
-    
-    return 0;
+        
+        // cout << srcAirportID << "  "<< destAirportID << "  ";
+    }
 }
-
-// void parseRoutes(ifstream &routes, map<string,string> &airportRoutes){
-//     vector<string> result;
-//     string line;
-//     string srcAirportID;
-//     string destAirportID;
-//     while(getline(routes, line)){
-//         result.clear();
-//         stringstream ss(line);
-//         while(getline(ss, line, ',')){
-//             result.push_back(line);
-//         }
-//         srcAirportID = result[2];
-//         destAirportID = result[4];
-
-        
-//         // cout << srcAirportID << "  "<< destAirportID << "  ";
-//     }
-// }
-
-// void parseAirports(ifstream &airnames, map<string, Airports> &airportData){
-//     vector<string> result;
-//     string line;
-//     string longitude;
-//     string latitude;
-//     string airportName;
-//     string iata;
-//     Airports newport;
-//     while(getline(airnames, line)){
-//         result.clear();
-//         stringstream ss(line);
-//         while(getline(ss, line, ',')){
-//             result.push_back(line);
-//         }
-//         longitude = result[6];
-//         latitude = result[7];
-//         airportName = result[1];
-//         iata = result[4];
-//         newport.longitude = longitude;
-//         newport.latitude = latitude;
-//         newport.airportName = airportName;
-//         newport.iata = iata;
-
-        
-//         airportData[airportName] = newport;
-//         //cout << longitude << "  "<< latitude << "  ";
-//     }
-// }
